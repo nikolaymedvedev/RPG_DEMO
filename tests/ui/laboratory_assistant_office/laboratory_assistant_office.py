28e76814fb35234e98047a2778b73944fe4d4d32
m.miadzvedzeu <sfff94@bk.ru>
2021-12-10 16:11:22 +0300

    Rename functions

--- tests/ui/laboratory_assistant_office/laboratory_assistant_office.py
+++ tests/ui/laboratory_assistant_office/laboratory_assistant_office.py
@@ -7,16 +7,16 @@ def test_checking_for_the_presence_of_elements_on_the_page(open_browser_chrome, 
     lab_page = LaboratoryAssistantOffice(open_browser_chrome, LaboratoryAssistantOffice.authorization_url)
     lab_page.open()
 
-    with allure.step("Авторизуемся от имени лаборанта"):
+    with allure.step("We log in on behalf of the laboratory assistant"):
         lab_page.login_to_the_page()
 
-    with allure.step("Проверяем наличие полей с персональными данными"):
+    with allure.step("We check the presence of fields with personal data"):
         lab_page.check_availability_of_fields_with_personal_data()
 
-    with allure.step("проверяем доступность переключаемых элементов"):
+    with allure.step("Checking the availability of switchable elements"):
         lab_page.check_availability_of_switchable_elements()
 
-    with allure.step("Проверяем доступность кнопок в скрытом меню"):
+    with allure.step("Check the availability of buttons in the hidden menu"):
         lab_page.click_enter_personal_account_button()
         lab_page.click_enter_list_of_participans_button()
         lab_page.click_enter_hidden_menu_button()
