c0ec78162b19f83e8da17ce045de7acfac5b20e2
Dzmitry Kashtanau <d.kashtanau@andersenlab.com>
2021-12-28 12:02:36 +0000

    Feature/rpg 455

---
+++ tests/ui/mentor_page/test_account_logout_is_confirmed.py
@@ -0,0 +1,21 @@
+import time
+import allure
+from app.ui.modules.mentor.mentor_page import MentorPage
+
+
+def test_account_logout_is_confirmed(open_browser_chrome, logger):
+    mentor_page = MentorPage(open_browser_chrome, MentorPage.authorization_url)
+    mentor_page.open()
+    mentor_page.should_be_authorization_page()
+
+    with allure.step("Mentor authorization"):
+        mentor_page.login_mentor_page()
+
+    with allure.step("Find my profile button"):
+        mentor_page.find_my_profile_button()
+
+    with allure.step("Find quit button"):
+        mentor_page.find_quit_button()
+
+    with allure.step("Select 'Yes'"):
+        mentor_page.select_yes()
